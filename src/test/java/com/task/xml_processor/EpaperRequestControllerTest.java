1a2789e039762f1b9015a78479fdead9ddcd0cf1
ravigautamtatvasoft <ravi.gautam@tatvasoft.com>
2024-10-22 13:52:01 +0530

    updated test case

--- src/test/java/com/task/xml_processor/EpaperRequestControllerTest.java
+++ src/test/java/com/task/xml_processor/EpaperRequestControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ClassPathResource;
@@ -16,9 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -45,10 +44,10 @@ public class EpaperRequestControllerTest {
         Path xmlFilePath = new ClassPathResource("correct-epaper-request.xml").getFile().toPath();
         String xmlContent = new String(Files.readAllBytes(xmlFilePath));
 
-        MockMultipartFile mockFile = new MockMultipartFile("paperXml", "correct-epaper-request.xml",
+        MockMultipartFile mockFile = new MockMultipartFile("xmlFile", "correct-epaper-request.xml",
                 MediaType.APPLICATION_XML_VALUE, xmlContent.getBytes());
 
-        Mockito.lenient().when(epaperService.processXml(any(), any())).thenReturn(ResponseEntity.ok().build());
+        when(epaperService.processXml(any(), any())).thenReturn(ResponseEntity.ok().build());
 
 
         mockMvc.perform(multipart("/api/processXml")
@@ -61,10 +60,10 @@ public class EpaperRequestControllerTest {
         Path xmlFilePath = new ClassPathResource("incorrect-epaper-request.xml").getFile().toPath();
         String xmlContent = new String(Files.readAllBytes(xmlFilePath));
 
-        MockMultipartFile mockFile = new MockMultipartFile("paperXml", "incorrect-epaper-request.xml",
+        MockMultipartFile mockFile = new MockMultipartFile("xmlFile", "incorrect-epaper-request.xml",
                 MediaType.APPLICATION_XML_VALUE, xmlContent.getBytes());
 
-        Mockito.lenient().when(epaperService.processXml(any(), any())).thenReturn(ResponseEntity.badRequest().build());
+        when(epaperService.processXml(any(), any())).thenReturn(ResponseEntity.badRequest().build());
 
         mockMvc.perform(multipart("/api/processXml")
                         .file(mockFile))
