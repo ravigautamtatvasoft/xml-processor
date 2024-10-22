e3d779742405a86660fac90097c7e8e10830a9a9
ravigautamtatvasoft <ravi.gautam@tatvasoft.com>
2024-10-22 12:43:59 +0530

    Initial setup of project

---
+++ src/main/java/com/task/xml_processor/XmlProcessorApplication.java
@@ -0,0 +1,15 @@
+package com.task.xml_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.InputStream;
+
+@SpringBootApplication
+public class XmlProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProcessorApplication.class, args);
+	}
+
+}
